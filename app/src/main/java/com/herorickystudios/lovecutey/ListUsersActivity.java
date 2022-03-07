8ea01415b43564063c2b9d298103bb2a8ea53277
HeroRickyGAMES <ricojn9@gmail.com>
2022-03-07 20:10:49 -0300

    Mudei a API das Swipes Dei uma pequena cleanada no codigo, creio que ainda tem muito para acrescentar

--- app/src/main/java/com/herorickystudios/lovecutey/ListUsersActivity.java
+++ app/src/main/java/com/herorickystudios/lovecutey/ListUsersActivity.java
@@ -3,7 +3,10 @@ package com.herorickystudios.lovecutey;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,12 +24,12 @@ import java.util.ArrayList;
 
 public class ListUsersActivity extends AppCompatActivity {
 
-    ArrayList<String> arrayListE;
-    ArrayAdapter arrayAdapter;
-    int n=0;
+    private ArrayList<String> al;
+    private ArrayAdapter<String> arrayAdapter;
+    private int i;
 
-    TextView nome, idade, cidade;
-    public DatabaseReference referencia = FirebaseDatabase.getInstance().getReference("Usuarios");
+
+    SwipeFlingAdapterView flingContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,86 +39,72 @@ public class ListUsersActivity extends AppCompatActivity {
         //Esconde a action Bar
         getSupportActionBar().hide();
 
-        nome = findViewById(R.id.textNomeUser);
-        idade = findViewById(R.id.textIdadeUser);
-        cidade = findViewById(R.id.textLocalização);
+        al = new ArrayList<>();
+        al.add("php");
+        al.add("c");
+        al.add("python");
+        al.add("java");
+        al.add("html");
+        al.add("c++");
+        al.add("css");
+        al.add("javascript");
 
-        usernamessc();
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.item, R.id.helloText, al);
+        //@InjectView(R.id.frame) SwipeFlingAdapterView flingContainer;
+        SwipeFlingAdapterView flingContainer = (SwipeFlingAdapterView) findViewById(R.id.frame);
 
-        arrayListE= new ArrayList<String >();
-        arrayListE.add("UM");
-        arrayListE.add("DOIS");
-        arrayListE.add("TRÊS");
-        arrayListE.add("QUAATRO");
-        arrayListE.add("RECARREGA");
-        SwipeFlingAdapterView swipeFlingAdapterView= (SwipeFlingAdapterView) findViewById(R.id.usersSwipe);
-
-
-    arrayAdapter= new ArrayAdapter<String>(this, R.layout.detalhes,R.id.exibir, arrayListE);
-    swipeFlingAdapterView.setAdapter(arrayAdapter);
-    swipeFlingAdapterView.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
-        @Override
-        public void removeFirstObjectInAdapter() {
-            arrayListE.remove(0);
-            arrayAdapter.notifyDataSetChanged();
-        }
-
-        @Override
-        public void onLeftCardExit(Object o) {
-            Toast.makeText(ListUsersActivity.this, "Movido para o lado", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onRightCardExit(Object o) {
-            Toast.makeText(ListUsersActivity.this, "Movido paro oto", Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onAdapterAboutToEmpty(int i) {
-
-        }
-
-        @Override
-        public void onScroll(float v) {
-
-        }
-    });
-
-    }
-
-    public void usernamessc(){
-
-        referencia.addValueEventListener(new ValueEventListener() {
+        flingContainer.setAdapter(arrayAdapter);
+        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                String refer = snapshot.getValue().toString();
-
-                //Ele está lendo os dados diretamente do Banco de Dados, ainda não fiz a lista, porem logo vou fazer!
-                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
-                    User user = dataSnapshot.getValue(User.class);
-                   String UIDO =  user.setUserID(dataSnapshot.getKey());
-
-                  String nomeud =  snapshot.child(UIDO).child("Dados do Usuario").child("nome").getValue().toString();
-                  String idadeud =  snapshot.child(UIDO).child("Dados do Usuario").child("idade").getValue().toString();
-
-                  nome.setText(nomeud);
-                  idade.setText(idadeud);
-
-                    System.out.println(nomeud);
-                }
-
-
-
+            public void removeFirstObjectInAdapter() {
+                // this is the simplest way to delete an object from the Adapter (/AdapterView)
+                Log.d("LIST", "removed object!");
+                al.remove(0);
+                arrayAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+            public void onLeftCardExit(Object dataObject) {
+                //Do something on the left!
+                //You also have access to the original object.
+                //If you want to use it just cast it (String) dataObject
+                Toast.makeText(ListUsersActivity.this, "Pu lado", Toast.LENGTH_SHORT).show();
+            }
 
+            @Override
+            public void onRightCardExit(Object dataObject) {
+                Toast.makeText(ListUsersActivity.this, "Pro oto", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdapterAboutToEmpty(int itemsInAdapter) {
+                // Ask for more data here
+                al.add("XML ".concat(String.valueOf(i)));
+                arrayAdapter.notifyDataSetChanged();
+                Log.d("LIST", "notified");
+                i++;
+            }
+
+            @Override
+            public void onScroll(float scrollProgressPercent) {
+                //View view = flingContainer.getSelectedView();
+                //view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
+                //view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
+            }
+        });
+
+
+        // Optionally add an OnItemClickListener
+        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClicked(int itemPosition, Object dataObject) {
+                Toast.makeText(ListUsersActivity.this, "Foi clicado!", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
 
-
+    static void makeToast(Context ctx, String s) {
+        Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
+     }
 }
